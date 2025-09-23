8e9816dcf427c4a257fdfdd7707bc1bdf7a28144
erickflorezny <erick.florez@gmail.com>
2025-09-23 16:59:07 -0400

    Fix build: Explicitly set rollup input to client/index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  root: "client",
   build: {
-    outDir: "../dist/public",
+    outDir: "dist/public",
     emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "client/index.html"),
+    },
   },
 });
