2c5b77c7b7d0348c0b2d1489e7accb2ebeda7a3b
erickflorezny <erick.florez@gmail.com>
2025-09-25 12:16:01 -0400

    Revert page files back to lowercase naming convention

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/home";
+import Home from "@/pages/Home";
 import Chat from "@/pages/chat";
 import About from "@/pages/about";
 import Windows from "@/pages/windows";
